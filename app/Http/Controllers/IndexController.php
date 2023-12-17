ad42e472a699f114fd64c4089c56035db8c6622a
Bakiweb <haiyassin1701@gmail.com>
2023-12-17 13:12:56 +0100

    [feature] WIP set up CRUD ressources for Curriculam

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Curriculam;
 use Illuminate\Http\Request;
 use Inertia\Response;
 
---
+++ resources/js/Pages/Curriculam/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Curriculam List</div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+defineProps({
+  message: String,
+})
+</script>
